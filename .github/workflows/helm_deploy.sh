8d26254f7f2cad958250873c150f4477a178cb3e
QJoly <qjoly@users.noreply.github.com>
2022-12-11 08:17:26 +0100

    :green_heart: Modification timeout helm de 10min à 2min

--- .github/workflows/helm_deploy.sh
+++ .github/workflows/helm_deploy.sh
@@ -2,7 +2,7 @@ cd ../../charts
 for d in *
 do
   echo "Deploying $d to kind"
-  (cd "$d" && helm install $d . --wait --timeout 600s && helm test $d )
+  (cd "$d" && helm install $d . --wait --timeout 120s && helm test $d )
   if [ $? -ne 0 ]; then
     echo "Error during deployment"
     exit 1
