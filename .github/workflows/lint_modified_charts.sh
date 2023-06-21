289c01e6b9dfa34368f5d6ec6294aeecde44ca5b
QJoly <github@thoughtless.eu>
2023-06-21 13:28:52 +0200

    Add dependency build before lint

--- .github/workflows/lint_modified_charts.sh
+++ .github/workflows/lint_modified_charts.sh
@@ -5,6 +5,7 @@ for chart in "$@"; do
   chart_name=$(echo "$chart" | cut -d"/" -f2 )
   echo "Chart Name: $chart_name"
   cd charts
+  helm dependency build $chart_name
   helm lint $chart_name
   if [ $? -ne 0 ]; then
     echo "Error in lint of $chart_name"
