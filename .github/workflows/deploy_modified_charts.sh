1e42d70b62b0d288e02d104c3b08755831939e6b
Quentin JOLY <82603435+QJoly@users.noreply.github.com>
2023-06-21 13:48:51 +0200

    Update CI to test only modified charts (#35)
    * Check which files have been edited
    
    * Change aptly
    
    * Script lint only modified chart
    
    * Using 'cut' to extract chart_name from path
    
    * Deploy to kind
    
    * test
    
    * Use right variable
    
    * test
    
    * Add more logs
    
    * Changes step names
    
    * suppression tests common
    
    * Add dependency build before lint
    
    * Add library charts
    
    * Apply tests methods to release CI
    
    * Delete tests
    
    * Do not create the kind cluster if no changes

---
+++ .github/workflows/deploy_modified_charts.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo $@
+basedir=$(pwd)
+for chart in "$@"; do
+  echo "---------------------------------------"
+  chart_name=$(echo "$chart" | cut -d"/" -f2 )
+  echo "Chart Name: $chart_name"
+  cd charts/$chart_name
+
+  if [ -f ".no_ci" ]; then
+      echo "No CI for this chart."
+  else
+      helm dependency build
+      helm install $chart_name . --wait --timeout 300s 
+      echo "$chart_name is installed"
+      helm test $chart_name
+  fi
+  
+  if [ $? -ne 0 ]; then
+    echo "Error during deployment"
+    exit 1
+  else
+    echo "Success ! "
+    helm delete $chart_name || true 
+  fi
+
+  if [ $? -ne 0 ]; then
+    echo "Error in lint of $chart_name"
+    exit 1
+  fi
+
+  cd $basedir
+done
+
