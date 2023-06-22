ba43dec2a38852b1ebf6a21bb4e3112c86726cc7
Quentin JOLY <82603435+QJoly@users.noreply.github.com>
2023-06-22 08:18:57 +0200

    smokeping: init at 2.8.2 (#36)
    * smokeping: init at 2.8.2
    
    * Fix image tag
    
    * smokeping: update common to 0.1.8
    
    * curl with different path
    
    * Disable curl test
    
    * test new CI that does not deploy twice the same chart
    
    * Remove duplicates from input list

--- .github/workflows/deploy_modified_charts.sh
+++ .github/workflows/deploy_modified_charts.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 echo $@
 basedir=$(pwd)
+echo "---------------------------------------"
+echo "Input: ${@}"
+echo "Extract chart names"
+edited_charts_list=()
 for chart in "$@"; do
+  chart_name=$(echo "$chart" | cut -d"/" -f2 )
+  edited_charts_list+=("$chart_name")
+done
+echo "Remove duplicates..."
+chart_list=($(printf "%s\n" "${edited_charts_list[@]}" | sort -u))
+echo "New list: ${chart_list[@]}"
+echo "---------------------------------------"
+
+for chart in "${chart_list[@]}"; do
   echo "---------------------------------------"
   chart_name=$(echo "$chart" | cut -d"/" -f2 )
   echo "Chart Name: $chart_name"
